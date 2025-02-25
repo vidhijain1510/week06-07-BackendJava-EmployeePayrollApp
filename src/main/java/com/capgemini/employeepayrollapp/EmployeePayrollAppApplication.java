6e4f46a5ae009a493d13894c9b72c9ab9411c552
Vidhi Jain <vidhijain507@gmail.com>
2025-02-25 13:17:53 +0530

    [Vidhi_Jain]Add. Database setting as Environment Variable.

--- src/main/java/com/capgemini/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/capgemini/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -11,5 +11,6 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		log.info("Employee Payroll Application has started successfully in environment: {}", context.getEnvironment().getProperty("environment"));
+		log.info("Employee payroll db_user is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 }
